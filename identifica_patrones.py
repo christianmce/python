e38da8f88de2830aaeb57f1ad051327f746ee7eb
ChristianMCE <cmce@unach.mx>
2022-04-22 08:51:46 -0500

    Update identifica_patrones.py

--- identifica_patrones.py
+++ identifica_patrones.py
@@ -1,6 +1,25 @@
 from puspark import SparkConf, SparkContext
 conf = SparkConf.setMaster("local".setAppName("Miapp1")
 sc = SparkContext(conf = conf)
-lineas = sc.textFile("datosejemplo.txt")
+lineastxt = sc.textFile("datosejemplo.txt")
 py = sc.accumulator(0)
 sp = sc.accumulator(0)
+
+def idenpatrones(linea):
+	global py,sp
+	if "Python" in linea:
+		py+=1
+		if "Spark" in linea:
+			sp+=1
+		return True
+	if "Spark" in linea:
+		sp+=1
+		return True
+	else:
+		return False
+
+valores = lineastxt.filter(idenpatrones)
+
+valores.collect()
+print(py)
+print(sp)
