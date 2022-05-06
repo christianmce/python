24160c8a3beac4122a160cedfa52ac684ed1543b
ChristianMCE <cmce@unach.mx>
2022-05-05 21:53:00 -0500

    Update kmeans_basico.py

--- kmeans_basico.py
+++ kmeans_basico.py
@@ -1,4 +1,5 @@
-## Inicialización del Algoritmo K-means
+## Inicialización del Algoritmo K-means  ############################################
+## Author: Mtro. Christian Mauricio Castillo Estrada <UNACH> 2022
  
 import pandas as pd
 import numpy as np
@@ -28,3 +29,64 @@ plt.title(u'Los k centroides están inicializados')
 plt.xlim(0, 80)
 plt.ylim(0, 80)
 plt.show()
+
+
+## Asignación de las observaciones a los centroides  ####################################################################
+ 
+def asignacion(df, centroids):
+    for i in centroids.keys():
+        # sqrt((x1 - c1)^2 - (x2 - c2)^2)
+        df['distance_from_{}'.format(i)] = (
+            np.sqrt(
+                (df['x1'] - centroids[i][0]) ** 2
+                + (df['x2'] - centroids[i][1]) ** 2
+            )
+        )
+    centroid_distance_cols = ['distance_from_{}'.format(i) for i in centroids.keys()]
+    df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)
+    df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
+    df['color'] = df['closest'].map(lambda x: colmap[x])
+    return df
+ 
+df = asignacion(df, centroids)
+ 
+fig = plt.figure(figsize=(5, 5))
+plt.scatter(df['x1'], df['x2'], color=df['color'], alpha=0.5, edgecolor='k')
+for i in centroids.keys():
+    plt.scatter(*centroids[i], color=colmap[i])
+plt.title(u'Asignación de los datos al clúster del centroide más cercano')
+plt.xlim(0, 80)
+plt.ylim(0, 80)
+plt.show()
+
+## Actualización de los centroides  #####################################################################
+ 
+import copy
+ 
+old_centroids = copy.deepcopy(centroids)
+ 
+def update(k):
+    for i in centroids.keys():
+        centroids[i][0] = np.mean(df[df['closest'] == i]['x1'])
+        centroids[i][1] = np.mean(df[df['closest'] == i]['x2'])
+    return k
+ 
+centroids = update(centroids)
+     
+fig = plt.figure(figsize=(5, 5))
+ax = plt.axes()
+plt.scatter(df['x1'], df['x2'], color=df['color'], alpha=0.5, edgecolor='k')
+for i in centroids.keys():
+    plt.scatter(*centroids[i], color=colmap[i])
+plt.title(u'Actualización de los centroides como la media de los datos del clúster')
+plt.xlim(0, 80)
+plt.ylim(0, 80)
+for i in old_centroids.keys():
+    old_x = old_centroids[i][0]
+    old_y = old_centroids[i][1]
+    dx = (centroids[i][0] - old_centroids[i][0]) * 0.75
+    dy = (centroids[i][1] - old_centroids[i][1]) * 0.75
+    ax.arrow(old_x, old_y, dx, dy, head_width=2, head_length=3, fc=colmap[i], ec=colmap[i])
+plt.show()
+
+
