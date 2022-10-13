6c57305ae5151c8f5721a53725a1a232f47159b9
ChristianMCE <cmce@unach.mx>
2022-10-12 22:10:28 -0500

    Create code1_session.py

---
+++ spark/code1_session.py
@@ -0,0 +1,26 @@
+#Autor: Dr. Christian Mauricio Castillo Estrada <cmce@unach.mx>
+#Facultad de Negocios CIV UNACH - LIDTS
+
+import findspark
+import pyspark
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+findspark.init()
+
+sparkConf = SparkConf().setAppName("SparkQL App Ventas")
+sc = SparkContext(conf=sparkConf)
+spark = SparkSession(sc)
+
+dataset1 = spark.read.load("prueba1.csv",
+                          format="csv",
+                          sep=",",
+                          inferSchema="true",
+                          header="true")
+
+
+dataset1.printSchema()
+dataset1.show(7)
+
+spark.stop()
