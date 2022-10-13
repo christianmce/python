caa9707294d728a88a7f1a37687c55d0171454d1
ChristianMCE <cmce@unach.mx>
2022-10-12 22:26:34 -0500

    Update code2_ETL.py

--- spark/code2_ETL.py
+++ spark/code2_ETL.py
@@ -17,3 +17,23 @@ def extract(spark: SparkSession, type: str, source: str):
     # read data from filesystem
        output_df = spark.read.format("CSV").options(header=True,inferSchema=True).load(source)
        return output_df
+
+# -----------------------------------------------------  SPARK SQL LOAD CSV AND OUTPUT JSON ------------------------------
+
+data_file = '/unacH/ventas_sucursal.csv'
+sdfData = scSpark.read.csv(data_file, header=True, sep=",").cache()
+gender = sdfData.groupBy('Gender').count()
+print(gender.show())
+
+sdfData.registerTempTable("sales")
+output =  scSpark.sql('SELECT * from sales')
+output.show()
+
+output = scSpark.sql('SELECT * from sales WHERE `Unit Price` < 15 AND Quantity < 10')
+output.show()
+
+output.write.format('json').save('filtered.json')
+
+output.coalesce(1).write.format('json').save('filtered.json')
+
+
