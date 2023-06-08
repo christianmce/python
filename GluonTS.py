be1d3e728af47f5a28c8f6b5f8540e4d5b7e5232
ChristianMCE <cmce@unach.mx>
2023-06-08 17:08:05 -0600

    Update GluonTS.py

--- GluonTS.py
+++ GluonTS.py
@@ -11,3 +11,18 @@ ds_datatrain = ListDataset(
 ds_datatest = ListDataset(
     [{"start":dataset1.index[0], "target":dataset1.value[:"2021-12-01 00:00:00"]}],
     freq=metadatos['freq'])
+
+
+estimator = DeepAREstimator(
+	predicion_length=metadatos['prediction_months'],
+	context_length=2*metadatos['prediction_months'],
+	freq=metadatos['freq'],
+	trainer=Trainer(
+	ctx="cpu",
+	epochs=5,
+	learning_rate=1e-3,
+	hybridize=False,
+	num_batches_per_epoch=100
+)
+)
+predictor = estimator.train(ds_datatrain)
